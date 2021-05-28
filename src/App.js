5cf4f06cd0fce284f20e95531cbbd5400839e1d7
Sma-source <rabhi.smain@gmail.com>
2021-05-28 19:25:54 +0200

    import Popup component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import "./App.css";
+import Popup from "./Popup";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [btnPop, setBtnPop] = useState(false);
+  const [timePop, setTimePop] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimePop(true);
+    }, 3000);
+  }, []);
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <main>
+        <h1>hello world</h1>
+        <button onClick={() => setBtnPop(true)}>Open Popup</button>
+        <Popup trigger={btnPop} setTrigger={setBtnPop}>
+          <h3>My Popup</h3>
+        </Popup>
+        <Popup trigger={timePop} setTrigger={setTimePop}>
+          <h3>My Time Popup</h3>
+        </Popup>
+      </main>
     </div>
   );
 }
