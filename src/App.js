2c6047459c94e2f918c2079c36f65361f5ed969f
Sma-source <rabhi.smain@gmail.com>
2021-06-26 23:35:45 +0200

    delete text in popup

--- src/App.js
+++ src/App.js
@@ -13,18 +13,10 @@ function App() {
   return (
     <div className="App">
       <main>
-        <h1>hello world</h1>
+        <h1>Pop-up</h1>
         <button onClick={() => setBtnPop(true)}>Open Popup</button>
-        <Popup trigger={btnPop} setTrigger={setBtnPop}>
-          <h3>My Time Popup</h3>
-        </Popup>
-        <Popup trigger={timePop} setTrigger={setTimePop}>
-          <h3>Le site respecte votre vie privée</h3>
-          <p className="text-popup">
-            Ce site utilise des cookies. Vous avez la possibilité de déterminer
-            les cookies que vous autorisez ou refusez.
-          </p>
-        </Popup>
+        <Popup trigger={btnPop} setTrigger={setBtnPop}></Popup>
+        <Popup trigger={timePop} setTrigger={setTimePop}></Popup>
       </main>
     </div>
   );
