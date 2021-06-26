c25fdd535a267db534fe4f6d9bc1e11ab2ab26e3
Sma-source <rabhi.smain@gmail.com>
2021-06-26 23:37:17 +0200

    update

--- src/Popup.js
+++ src/Popup.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const Popup = ({ trigger, setTrigger, children }) => {
   return trigger ? (
-    <div className="popup">
-      <div className="popup-inner">
+    <div className="wrapper">
+      <div className="popup">
         {children}
-        <div className="btn-contain">
+        <h3 className="title-popup">Le site respecte votre vie privée</h3>
+        <p className="text-popup">
+          Ce site utilise des cookies. Vous avez la possibilité de déterminer
+          les cookies que vous autorisez ou refusez.
+        </p>
+        <div className="btn">
           <button className="close-btn" onClick={() => setTrigger(false)}>
             Accepter
           </button>
